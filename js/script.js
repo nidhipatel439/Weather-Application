d84df1df6e28fead730ec9a9d556c85812c3e436
Nidhi <nidhipatel439@gmail.com>
2022-04-20 22:12:54 -0400

    merge all css and changes

--- js/script.js
+++ js/script.js
@@ -23,54 +23,94 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-var searchCity = document.getElementById("search-city");
-var searchButton = document.getElementById("search-button");
-var cityName = document.getElementById("city-name");
-var cityTemp = document.getElementById("city-temp");
-var errorMessage = document.getElementById("error-message");
-var weatherColumn = document.getElementById("weather-column");
+// var searchCity = document.getElementById("search-city");
+// var searchButton = document.getElementById("search-button");
+// var cityName = document.getElementById("city-name");
+// var cityTemp = document.getElementById("city-temp");
+// var errorMessage = document.getElementById("error-message");
+// var weatherColumn = document.getElementById("weather-column");
 
-//create a variable for api key
+// //create a variable for api key
 
-//create a function for api request and response
-function apiCall(city) {
-  //create a variable for the url
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    city +
-    "&appid=" +
-    apiKey +
-    "&units=metric";
+// //create a function for api request and response
+// function apiCall(city) {
+//   //create a variable for the url
+//   var url =
+//     "https://api.openweathermap.org/data/2.5/weather?q=" +
+//     city +
+//     "&appid=" +
+//     apiKey +
+//     "&units=metric";
 
-  var xhr = new XMLHttpRequest();
+//   var xhr = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var data = xhr.response;
-        console.log(data);
-        cityName.innerHTML = data.name;
-        cityTemp.innerHTML = data.main.temp + "&#8451;";
-        conditions.innerHTML = data.weather[0].main;
-        sunsetSpan.innerHTML = new Date(data.sys.sunset).toTimeString();
-      } else {
-        cityName.innerHTML = "API was unsuccessful";
-        console.log(xhr.status);
-      }
-    }
-  };
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         var data = xhr.response;
+//         console.log(data);
+//         cityName.innerHTML = data.name;
+//         cityTemp.innerHTML = data.main.temp + "&#8451;";
+//         conditions.innerHTML = data.weather[0].main;
+//         sunsetSpan.innerHTML = new Date(data.sys.sunset).toTimeString();
+//       } else {
+//         cityName.innerHTML = "API was unsuccessful";
+//         console.log(xhr.status);
+//       }
+//     }
+//   };
 
-  //url and respond type
-  xhr.open("GET", url, true);
-  xhr.responseType = "json";
-  xhr.send(null);
-}
+//   //url and respond type
+//   xhr.open("GET", url, true);
+//   xhr.responseType = "json";
+//   xhr.send(null);
+// }
 
-searchButton.addEventListener("click", function () {
-  if (searchCity.value === "") {
-    errorMessage.classList.remove("hide");
-  } else {
-    errorMessage.classList.add("hide");
-    apiCall(searchCity.value);
-  }
-});
+// searchButton.addEventListener("click", function () {
+//   if (searchCity.value === "") {
+//     errorMessage.classList.remove("hide");
+//   } else {
+//     errorMessage.classList.add("hide");
+//     apiCall(searchCity.value);
+//   }
+// });
+
+var fullPageBackground = document.getElementById("full-page");
+// var row = document.getElementById("weather-column");
+var snowColumn = document.getElementById("snow-column");
+var blockSnow = document.createElement("div");
+
+var rainyColumn = document.getElementById("rainy-column");
+var sunnyColumn = document.getElementById("sunny-column");
+
+snowColumn.onmouseover = function () {
+  fullPageBackground.classList.add("background-snow");
+  fullPageBackground.classList.remove("background-rain");
+  fullPageBackground.classList.remove("background-sunny");
+  blockSnow.classList.add("block-snow");
+  fullPageBackground.appendChild(blockSnow);
+};
+snowColumn.onmouseout = function () {
+  fullPageBackground.classList.remove("background-snow");
+  blockSnow.classList.remove("block-snow");
+};
+
+rainyColumn.onmouseover = function () {
+  fullPageBackground.classList.add("background-rain");
+  fullPageBackground.classList.remove("background-snow");
+  fullPageBackground.classList.remove("background-sunny");
+  blockSnow.classList.remove("block-snow");
+};
+rainyColumn.onmouseout = function () {
+  fullPageBackground.classList.remove("background-rain");
+};
+
+sunnyColumn.onmouseover = function () {
+  fullPageBackground.classList.add("background-sunny");
+  fullPageBackground.classList.remove("background-snow");
+  fullPageBackground.classList.remove("background-rain");
+};
+
+sunnyColumn.onmouseout = function () {
+  fullPageBackground.classList.remove("background-sunny");
+};
